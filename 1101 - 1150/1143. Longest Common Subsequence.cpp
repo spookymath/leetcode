1e63f2365ff8ca91ae3cb510b7e7ab122dec0584
spookymath <104441168+spookymath@users.noreply.github.com>
2022-07-27 14:53:50 +0530

    Create 1143. Longest Common Subsequence.cpp

---
+++ 1101 - 1150/1143. Longest Common Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) 
+    {
+        int m = 0;
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
+        
+        for(int i=0; i<dp.size(); i++) dp[i][0] = 0;
+        for(int j=0; j<dp[0].size(); j++) dp[0][j] = 0;
+        
+        for(int i=1; i<dp.size(); i++)
+            for(int j=1; j<dp[0].size(); j++)
+            {
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                m = max(m, dp[i][j]);
+            }
+        
+        return m;
+        
+    }
+};
